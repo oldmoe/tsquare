1f117beb8e4c037e13eda404efc4b55abc51f705
Riham <riham.aldakkak@gmail.com>
2011-09-28 20:22:44 +0200

    only serve data if mission is playable

--- app/models/user_missions.rb
+++ app/models/user_missions.rb
@@ -15,8 +15,9 @@ class UserMissions
 
     def data user_profile, mission_id
       data = {}
+      mission_id = mission_id.to_i
       mode = Mission.mode mission_id
-      if user_profile.missions[mode][mission_id]
+      if user_profile.current_mission[mode]==mission_id ||user_profile.missions[mode][mission_id]
         data = Mission.get(mission_id)
       end
       data

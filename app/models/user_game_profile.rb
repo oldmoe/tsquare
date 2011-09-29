5050a2bc068b35f343a62aad7f69e1d0d79d9fb5
Michael Youssef <michaelyta@gmail.com>
2011-09-29 18:08:06 +0300

    updating game data and current version

--- app/models/user_game_profile.rb
+++ app/models/user_game_profile.rb
@@ -1,7 +1,7 @@
 class UserGameProfile < DataStore::Model
 
   SEP = '-'.freeze
-  CURRENT_VERSION = 2
+  CURRENT_VERSION = 3
 
   index :timeline_score, :method => :timeline_index
   index :racing_score, :method => :racing_index

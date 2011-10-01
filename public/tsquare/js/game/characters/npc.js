30fc4caf6e97dd8710e7f561084434057122f037
nomiero <hussein_nomier2010@yahoo.com>
2011-10-01 19:07:10 +0200

    fixing npcs

--- public/tsquare/js/game/characters/npc.js
+++ public/tsquare/js/game/characters/npc.js
@@ -1,5 +1,10 @@
 var Npc = Class.create(Unit,{
     speed : 0,
+    initialize : function($super,scene,x,lane, options){
+      $super(scene,x,lane, options)
+      this.coords.x+=Math.round(Math.randomSign()*Math.random()*this.scene.view.tileWidth/4)
+      this.coords.y+=Math.random()*60
+    },
     tick : function($super){
       $super()
       this.move(-(this.speed+ this.scene.currentSpeed*this.scene.direction),0)

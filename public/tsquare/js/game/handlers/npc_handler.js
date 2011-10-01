ef322cf2c7e0ea9052559e4668c112a28571a7da
nomiero <hussein_nomier2010@yahoo.com>
2011-10-01 18:20:42 +0200

    fixing npcs

--- public/tsquare/js/game/handlers/npc_handler.js
+++ public/tsquare/js/game/handlers/npc_handler.js
@@ -1,7 +1,7 @@
 var NPCHandler = Class.create(UnitHandler,{
   initialize: function($super,scene){
     $super(scene)
-    this.unitsClassMappings = {'normal_man':'normal','doctor':'medic'}
+    this.unitsClassMappings = {'normal_man':'normal','doctor':'medic','girl_7egab':'girl7egab'}
   },
    addObject: function($super, obj){
     obj.options.type = obj.name

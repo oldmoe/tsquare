bdced83fc0338bbf95cf429cf8301f41cff2c578
nomiero <hussein_nomier2010@yahoo.com>
2011-10-03 22:15:19 +0200

    new backgrounds

--- public/tsquare/js/game/sky_line.js
+++ public/tsquare/js/game/sky_line.js
@@ -12,17 +12,17 @@ var SkyLine = Class.create({
       var images_fence = [Loader.images.background[game.data.backgrounds.fence[0].name]]
         
       var self = this
-      var background_cloud = new Background(this.scene, {speed : function(){return  self.scene.direction*(0)}, y: 0, imagesCount: 1, images:images_cloud, offsetX:0});
+      var background_cloud = new Background(this.scene, {speed : function(){return  self.scene.direction*(0)}, y: 0, imagesCount: 15, images:images_cloud, offsetX:0});
       var background_layer1 = new Background(this.scene, {speed : function(){return  self.scene.direction*(self.scene.currentSpeed-2)}, y: 0, imagesCount: 3, images:images_layer1})
       var background_layer2 = new Background(this.scene, {speed : function(){return  self.scene.direction*(self.scene.currentSpeed-1)}, y: 65, imagesCount: 3, images:images_layer2})
       var background_road = new Background(this.scene, {speed : function(){return  self.scene.direction*(self.scene.currentSpeed)}, y: 340, imagesCount: 2, offsetX:0, images:images_road})
       var background_fence = new Background(this.scene, {speed : function(){return  self.scene.direction*(self.scene.currentSpeed)}, y: 350, imagesCount: 14, images:images_fence})
       var background_lamp = new Background(this.scene, {speed : function(){return  self.scene.direction*(self.scene.currentSpeed)}, y: 200, imagesCount: 3, images:images_lamp})
       
-      var transparent_layer1 = new TransparentLayer(background_cloud.container);
-      var transparent_layer2 = new TransparentLayer(background_layer2.container);
-      transparent_layer1.setBackgroundColor("ff2200");
-      transparent_layer2.setBackgroundColor("ffcccc");
+//      var transparent_layer1 = new TransparentLayer(background_cloud.container);
+//      var transparent_layer2 = new TransparentLayer(background_layer2.container);
+//      transparent_layer1.setBackgroundColor("ff2200");
+//      transparent_layer2.setBackgroundColor("ffcccc");
       
       this.backgrounds.push(background_cloud)
       this.backgrounds.push(background_layer1)

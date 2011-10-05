d1a7e163e549e0a8a0a0250a7983929c23b8c625
Ahmed Abousamra <ahmed.abousamra87@gmail.com>
2011-10-05 19:40:59 +0200

    energy bar

--- public/tsquare/js/game/ingame_meter_bar.js
+++ public/tsquare/js/game/ingame_meter_bar.js
@@ -4,12 +4,9 @@ var InGameMeterBar = Class.create({
     this.game = game;
     this.templateManager = game.templateManager;
     this.scoreCalculator = game.scene.scoreCalculator;
-
-    this.currentEnergyWidth = 0;
-    this.currentEnergy = 0;
-  
+    this.currentEnergy = -1;
     $('inGameMeterBar').innerHTML = this.templateManager.load('inGameMeterBar');
-
+    this.energyBarWidth = parseInt($$('.inGameMeterBar .energyBar .bar')[0].getStyle('width').split('px')[0]);
     var self = this;
     game.scene.reactor.pushEvery(0, game.scene.reactor.everySeconds(1), function(){self.tick();});
   },
@@ -25,19 +22,14 @@ var InGameMeterBar = Class.create({
     var seconds = time[2]<10?"0"+time[2]:time[2];
     $('timer').innerHTML = hours + ":" + minutes + ":" + seconds;
     
-    // if(this.currentEnergy != this.game.scene.energy.current){
-      // this.currentEnergy = this.game.scene.energy.current;
-      // $$('.energyBar span')[0].innerHTML = Math.floor(this.game.scene.energy.current);
-      // var energyBarFill = (Math.floor(this.game.scene.energy.current)/this.game.scene.energy.max) * this.energyBarWidth;
-//       
-    // }
-//     
-//     
-//     
-    // $$('.energyBar .bar table')[0].setStyle({ 'width' : energyBarFill } );
-    // if(energyBarFill == 0) $$('.energyBar .bar table')[0].hide();
-//     
-    
-    $('energy').innerHTML = this.game.scene.energy.current;
+    if(this.currentEnergy != this.game.scene.energy.current){
+      this.currentEnergy = this.game.scene.energy.current;
+      $$('.inGameMeterBar .energyBar span')[0].innerHTML = Math.floor(this.game.scene.energy.current);
+      var energyBarFill = (Math.floor(this.game.scene.energy.current)/this.game.scene.energy.max) * this.energyBarWidth;
+      console.log(energyBarFill);
+      $$('.inGameMeterBar .energyBar .bar table')[0].setStyle({ 'width' : energyBarFill } );
+      if(energyBarFill == 0) $$('.inGameMeterBar .energyBar .bar table')[0].hide();
+      else $$('.inGameMeterBar .energyBar .bar table')[0].show();
+    }
   }
 });

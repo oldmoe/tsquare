91e53df32b62e71d9947efefd86ed1e79b36663a
Riham <riham.aldakkak@gmail.com>
2011-10-06 16:42:29 +0200

    properly initialize bought crowd members

--- app/models/marketplace.rb
+++ app/models/marketplace.rb
@@ -7,10 +7,10 @@ class Marketplace
       end
       if user_game_profile['crowd_members'][name]
         next_id = user_game_profile['crowd_members'][name].keys.max + 1
-        user_game_profile['crowd_members'][name][next_id] = {}
+        user_game_profile['crowd_members'][name][next_id] = {'level' => 1}
       else
         user_game_profile['crowd_members'][name] = {}
-        user_game_profile['crowd_members'][name][1] = {}
+        user_game_profile['crowd_members'][name][1] = {'level' => 1}
       end
       if cost_in_coins
         user_game_profile.user.coins -= 10

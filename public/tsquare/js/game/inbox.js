be08d9d451a121ba55e8299f777674c2951bce2b
Riham <riham.aldakkak@gmail.com>
2011-10-11 13:32:24 +0200

    notifications

--- public/tsquare/js/game/inbox.js
+++ public/tsquare/js/game/inbox.js
@@ -1,15 +1,18 @@
 var Inbox = Class.create({
 
-  requests : {help : [], gift : [], challenge : []},
+  requests : {},
   
   initialize : function(gameManager){
     this.network = gameManager.network;    
     this.templateManager = gameManager.templateManager;
+    this.gameManager = gameManager;
     var self = this;
     new Loader().load([ {images : ["notifications_item.png", "title_inbox.png", "button_cancel.png", "confirm_button.png"],
                           path: 'images/notifications/', store: 'notifications' }, 
-                        {images : ["close_button.png", ""], 
-                          path: 'images/game_elements/', store: 'game_elements' },
+                        {images : ["window_background.png"], 
+                          path: 'images/marketplace/', store: 'marketplace' },
+                        {images : ["close_button.png"], 
+                          path: 'images/game_elements/', store: 'game_elements' }
                       ],
                       {
                         onFinish: function(){
@@ -20,17 +23,67 @@ var Inbox = Class.create({
     this.getInboxRequests();
   },
 
+  show : function(){
+    $('notifications').show();
+  },
+
+  hide : function(){
+    $('notifications').hide();
+  },
+
   display : function(){
+    var self = this;
     if(this.imagesLoaded && this.dataLoaded)
     {
       $('notifications').innerHTML = this.templateManager.load('notifications', {inbox : this});
+      $$('.notifications .close a').each(function(button){
+        button.observe('click',  function(){self.hide()});
+      });
+      this.displayList();
+      Game.addLoadedImagesToDiv('notifications');
     }    
   },
 
+  displayList : function(){
+    var self = this;
+    this.sortRequests();
+    if(this.sortedRequests.length == 0)
+    {
+      this.hide();
+      return;
+    }
+    $$('#notifications .notificationWrapper')[0].innerHTML = this.templateManager.load('notificationsList', 
+            {list : this.sortedRequests.slice(0, 3)});
+    var userIDs = this.sortedRequests.slice(0, 3).collect(function(request){return request.from.id});
+    socialEngine.getUsersInfo(userIDs, function(users){
+      users.each(function(user){
+        $$('.notificationItem .notificationImage img').each(function(image){
+          if(image.id == user.uid) 
+          {
+            image.src = user.pic_square;
+          }
+        });
+      })
+    });
+    $$('.notificationActions .confirmButton').each(function(button){
+      button.observe('click',  function(){self.acceptRequest(button.getAttribute('requestId'))});
+    });
+    $$('.notificationActions .cancelButton').each(function(button){
+      button.observe('click',  function(){self.cancelRequest(button.getAttribute('requestId'))});
+    });
+  },
+
   acceptRequest : function(requestId){
+    this.network.genericPostRequest('requests/accept', {request_id : requestId, from : this.requests[requestId]['from']['id']});
+    delete(this.requests[requestId]);
+    var self = this;
+    socialEngine.deleteObject(requestId, function(){ self.displayList() });
   },
   
   cancelRequest : function(requestId){
+    delete(this.requests[requestId]);
+    var self = this;
+    socialEngine.deleteObject(requestId, function(){ self.displayList() });
   },
   
   requestTrigger : function(requestType){
@@ -52,11 +105,27 @@ var Inbox = Class.create({
       requests.each(function(request){
         var data = JSON.parse( request.data )
         //console.log(request)
-        self.requests[ data.type ].push( request );
+        self.requests[request.id] = request;
       }); 
       self.dataLoaded = true;
+      self.noOfRequests = requests.length;
       self.display();
     });
+  },
+
+  sortRequests : function(){
+    var self = this;
+    this.sortedRequests = [];
+    for(var i in this.requests)
+    {
+      this.sortedRequests.push(this.requests[i]);
+    }    
+    this.sortedRequests = this.sortedRequests.sortBy(function(request){ 
+      if(self.gameManager.notifyFirst && self.gameManager.notifyFirst.indexOf(request.id) > -1) 
+        return 0;
+      else
+        return request.id 
+    }).reverse();
   }
 
 });

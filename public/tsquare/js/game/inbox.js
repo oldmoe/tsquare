f0eeec60defb7fa46dc3fa43a3554b31fda09d9c
Riham <riham.aldakkak@gmail.com>
2011-10-13 20:15:41 +0200

    add move up effect on notification accept

--- public/tsquare/js/game/inbox.js
+++ public/tsquare/js/game/inbox.js
@@ -1,18 +1,22 @@
 var Inbox = Class.create({
 
   requests : {},
-  
+
+
   initialize : function(gameManager){
     this.network = gameManager.network;    
     this.templateManager = gameManager.templateManager;
     this.gameManager = gameManager;
+    this.usersInfo = null;
     var self = this;
     this.gameManager.loader.load([ {images : ["notifications_item.png", "title_inbox.png", "button_cancel.png", "confirm_button.png"],
                           path: 'images/notifications/', store: 'notifications' }, 
                         {images : ["window_background.png"], 
                           path: 'images/marketplace/', store: 'marketplace' },
-                        {images : ["close_button.png"], 
-                          path: 'images/game_elements/', store: 'game_elements' }
+                        {images : ["close_button.png"],
+                          path: 'images/game_elements/', store: 'game_elements' },
+                        {images : ["facebook_image_large.png"],  
+                          path: 'images/dummy/', store: 'dummy' }
                       ],
                       {
                         onFinish: function(){
@@ -53,10 +57,24 @@ var Inbox = Class.create({
       return;
     }
     $$('#notifications .notificationWrapper')[0].innerHTML = this.templateManager.load('notificationsList', 
-            {list : this.sortedRequests.slice(0, 3)});
-    var userIDs = this.sortedRequests.slice(0, 3).collect(function(request){return request.from.id});
-    socialEngine.getUsersInfo(userIDs, function(users){
-      users.each(function(user){
+            {list : this.sortedRequests});
+    Game.addLoadedImagesToDiv('notificationWrapper');
+    if(!this.usersInfo)
+    {
+      var userIDs = this.sortedRequests.slice(0, 3).collect(function(request){return request.from.id});
+      socialEngine.getUsersInfo(userIDs, function(users){
+        self.usersInfo = users;
+        users.each(function(user){
+          $$('.notificationItem .notificationImage img').each(function(image){
+            if(image.id == user.uid) 
+            {
+              image.src = user.pic_square;
+            }
+          });
+        })
+      });
+    }else{
+      this.usersInfo.each(function(user){
         $$('.notificationItem .notificationImage img').each(function(image){
           if(image.id == user.uid) 
           {
@@ -64,7 +82,7 @@ var Inbox = Class.create({
           }
         });
       })
-    });
+    }
     $$('.notificationActions .confirmButton').each(function(button){
       button.observe('click',  function(){self.acceptRequest(button.getAttribute('requestId'))});
     });
@@ -77,7 +95,13 @@ var Inbox = Class.create({
     this.network.genericPostRequest('requests/accept', {request_id : requestId, from : this.requests[requestId]['from']['id']});
     delete(this.requests[requestId]);
     var self = this;
-    socialEngine.deleteObject(requestId, function(){ self.displayList() });
+    new Effect.BlindUp($$('.notificationWrapper')[0].select(".notificationItem[requestId=" + requestId + "]")[0], 
+                        { afterFinish: function(){
+                            self.displayList();
+                          }, 
+                          duration : 0.3
+                        });
+    socialEngine.deleteObject(requestId);
   },
   
   cancelRequest : function(requestId){
@@ -103,8 +127,7 @@ var Inbox = Class.create({
     var self = this;
     socialEngine.getAppRequests(function(requests){
       requests.each(function(request){
-        var data = JSON.parse( request.data )
-        //console.log(request)
+        request.data = JSON.parse( request.data )
         self.requests[request.id] = request;
       }); 
       self.dataLoaded = true;

0adde4ed7b1be25fb8b5a17f064689b190600b1f
Ahmed Abousamra <ahmed.abousamra87@gmail.com>
2011-10-20 16:11:56 +0200

    add shadow to npc

--- public/tsquare/js/game/display/npc_display.js
+++ public/tsquare/js/game/display/npc_display.js
@@ -1,12 +1,26 @@
 var NpcDisplay = Class.create(Display,{
-  noOfFrames : 8, 
+  
+  noOfFrames : 8,
+   
   initialize : function($super,owner,properties){
     this.characterImg = Loader.images.characters[owner.type+'_walk.png'];
     this.imgWidth = this.characterImg.width
     this.imgHeight = this.characterImg.height / this.noOfFrames
     $super(owner)
   },
+  
+  createShadow: function(){
+    this.shadowImg = Loader.images.effects['crowd_shadow.png'];
+    this.sprites.shadow = new DomImgSprite(this.owner, {img : this.shadowImg,noOfFrames : 1}, {
+      width: this.shadowImg.width,
+      height: this.shadowImg.height,
+      shiftX : -(this.shadowImg.width-this.characterImg.width)-10,
+      shiftY : -10
+    })    
+  },
+  
   createSprites : function(){
+      this.createShadow();
       if (this.owner.direction == -1) 
         this.sprites.character = new DomImgSprite(this.owner, {
           img: this.characterImg,
@@ -21,6 +35,7 @@ var NpcDisplay = Class.create(Display,{
         })
       }
   },
+  
   render : function($super){
       this.sprites.character.currentAnimationFrame = (this.sprites.character.currentAnimationFrame+1) % this.noOfFrames
       if(this.owner.directionReversed){

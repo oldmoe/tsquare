62b9d0c85e767affe1211b4c74ad4a35da897bdb
Michael Youssef <michaelyta@gmail.com>
2012-01-15 12:44:47 +0200

    fixing a problem with objectives, where when a mission with no objectives, it always fails

--- public/tsquare/js/game/tsquare_scene.js
+++ public/tsquare/js/game/tsquare_scene.js
@@ -225,10 +225,7 @@ var TsquareScene = Class.create(Scene,{
         this.stopped = true;
         
         var scoreData = {};
-        
-        if(this.handlers.crowd.ended || this.scoreCalculator.gameTime < 0 || this.scoreCalculator.getObjectivesRatio() < 0.3){
-          
-          scoreData = {
+        var failingScoreData = {
             score: 0,
             objectives: this.scoreCalculator.getObjectivesRatio().toFixed(2),
             combos: this.scoreCalculator.getCombos(),
@@ -236,21 +233,27 @@ var TsquareScene = Class.create(Scene,{
             superTime: 0,
             stars: 0
           };
-
+        
+        if(this.handlers.crowd.ended || this.scoreCalculator.gameTime < 0 ){
+          scoreData = failingScoreData;
         }else{
           var superTime = false;
           if(this.scoreCalculator.gameTime > this.scoreCalculator.superTime) superTime = true;
           
           this.fire('correctObjective'); //Ending the mission alive is a correct objective
           
-          scoreData = {
-            score: this.scoreCalculator.score,
-            objectives: this.scoreCalculator.getObjectivesRatio().toFixed(2),
-            combos: this.scoreCalculator.getCombos(),
-            win: true,
-            superTime: superTime,
-            stars: 0
-          };
+          if( this.scoreCalculator.getObjectivesRatio() < 0.3 ){
+            scoreData = failingScoreData;
+          } else {
+            scoreData = {
+              score: this.scoreCalculator.score,
+              objectives: this.scoreCalculator.getObjectivesRatio().toFixed(2),
+              combos: this.scoreCalculator.getCombos(),
+              win: true,
+              superTime: superTime,
+              stars: 0
+            };
+          }
           
           if (scoreData.win) scoreData.stars += 1;
           if (scoreData.objectives == 1)scoreData.stars += 1;

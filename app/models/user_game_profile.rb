50d2a2b52f91e7a8c133bb2e35a97f3042d1f257
nomier <hussein.nomier@gamil.com>
2012-02-05 12:17:17 +0200

    incrementing user_game_profile version

--- app/models/user_game_profile.rb
+++ app/models/user_game_profile.rb
@@ -1,7 +1,7 @@
 class UserGameProfile < DataStore::Model
 
   SEP = '-'.freeze
-  CURRENT_VERSION = 51
+  CURRENT_VERSION = 52
 
   index :timeline_score, :method => :timeline_index
   index :racing_score, :method => :racing_index
